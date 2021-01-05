38bee671f9280af88e94f3012f321e61abf29275
Aidan Lalonde-Novales <aidan.lalonde-novales@ocsbstudent.ca>
2021-01-05 15:01:41 +0000

    fixed errors lol emoji face

--- script.js
+++ script.js
@@ -3,12 +3,12 @@
 document.getElementById('submitbutton').addEventListener('click', numberChecker) // Executes the function numberChecker by clickign the "Submit" button.
 
 function numberChecker () {
- const preUserAge = document.getElementById('userage').value // Makes the value of the age textbox into a variable
- const userAge = parseInt(preUserAge) // Converts to interger
- const userDay = document.getElementById('userday').value // Makes the value of the day of the week textbox into a variable
- if ((userDay == 'Tuesday' || userDay == 'Thursday') || userAge > 12 && userAge < 21) { // Checks to see if the day is Tuesday or Thursday or if the users age 
-   document.getElementById('result').innerHTML = 'You are eligible for student pricing!' // If one of the above statements is true, this will display.
- } else {
-   document.getElementById('result').innerHTML = 'You must pay normal price.' // If the above statements are false, this will display.
- }
+  const preUserAge = document.getElementById('userage').value // Makes the value of the age textbox into a variable.
+  const userAge = parseInt(preUserAge) // Converts preUserAge to an interger.
+  const userDay = document.getElementById('userday').value // Makes the value of the day of the week textbox into a variable.
+  if ((userDay === 'Tuesday' || userDay === 'Thursday') || (userAge > 12 && userAge < 21)) { // Checks to see if the day is Tuesday or Thursday or if the users age.
+    document.getElementById('result').innerHTML = 'You are eligible for student pricing!' // If one of the above statements is true, this will display.
+  } else {
+    document.getElementById('result').innerHTML = 'You must pay normal price.' // If the above statements are false, this will display.
+  }
 }
